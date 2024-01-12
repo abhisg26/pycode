0a59670ce0cae96e64646fd8e90df1965256b277
abhisg26 <abhisg26@gmail.com>
2024-01-12 20:52:29 +0530

    Main module run

---
+++ MustKnowAboutPython/start.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    return a+b
+
+if __name__=="__main__": # Very important because it will only run if the usecase needs this module to run when the method add is imported it only executes add operation while "run" will only be printed when this code will be run
+    print("run")
